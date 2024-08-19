7c78644ca9a55e74e5214e437509865cad6f904d
AmiahBelleIsle <amiah.belleisle@proton.me>
2024-08-18 23:24:59 -0500

    Updated dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module belleisle.amiah.hardwarespecget {
 
     requires org.controlsfx.controls;
     requires java.xml;
+    requires com.github.oshi;
+    requires com.fasterxml.jackson.jr.ob;
 
     opens belleisle.amiah.hardwarespecget to javafx.fxml;
     exports belleisle.amiah.hardwarespecget;
