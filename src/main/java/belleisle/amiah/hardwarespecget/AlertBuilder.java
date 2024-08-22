f645682f0cff0ec985413fce88d646f191b4cede
AmiahBelleIsle <amiah.belleisle@proton.me>
2024-08-22 12:43:05 -0500

    Added document comments

--- src/main/java/belleisle/amiah/hardwarespecget/AlertBuilder.java
+++ src/main/java/belleisle/amiah/hardwarespecget/AlertBuilder.java
@@ -9,29 +9,75 @@ public class AlertBuilder {
     private String messageText;
     private String headerText;
 
+    /**
+     * Creates a new AlertBuilder Instance. It may be more
+     * convenient to use AlertBuilder.makeBuilder() instead.
+     *
+     * @param type The alert type
+     * @see Alert
+     * @see javafx.scene.control.Alert.AlertType
+     */
     public AlertBuilder(Alert.AlertType type) {
         this.type = type;
     }
 
+    /**
+     * Creates a new AlertBuilder Instance
+     *
+     * @param type The alert type
+     * @return A new AlertBuilder instance
+     * @see Alert
+     * @see javafx.scene.control.Alert.AlertType
+     */
     public static AlertBuilder makeBuilder(Alert.AlertType type) {
         return new AlertBuilder(type);
     }
 
+    /**
+     * Sets the text of the title bar. If unset its default value is "Message".
+     *
+     * @param windowTitle The text to be displayed on the title bar
+     * @return this
+     * @see Alert
+     */
     public AlertBuilder setWindowTitle(String windowTitle) {
         this.windowTitle = windowTitle;
         return this;
     }
 
+    /**
+     * Sets the heading text. This is the large text above the message.
+     * If unset its default value is "Message".
+     *
+     * @param headerText The text to be displayed as the header
+     * @return this
+     * @see Alert
+     */
     public AlertBuilder setHeaderText(String headerText) {
         this.headerText = headerText;
         return this;
     }
 
+    /**
+     * Sets the message text. This is the small text under the heading.
+     * If unset its default value is an empty string.
+     *
+     * @param messageText The text to be displayed as the message
+     * @return this
+     * @see Alert
+     */
     public AlertBuilder setMessage(String messageText) {
         this.messageText = messageText;
         return this;
     }
 
+    /**
+     * Constructs and returns an Alert. This method does not
+     * show the Alert it returns.
+     *
+     * @return Alert
+     * @see Alert
+     */
     public Alert build() {
         Alert alert = new Alert(type);
 
