1bcabd1a251b358b2da85751735df5b87b6af00f
AmiahBelleIsle <amiah.belleisle@proton.me>
2024-08-21 20:28:28 -0500

    switched out jackson jr for regular jackson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@ module belleisle.amiah.hardwarespecget {
     requires org.controlsfx.controls;
     requires java.xml;
     requires com.github.oshi;
-    requires com.fasterxml.jackson.jr.ob;
+    requires com.fasterxml.jackson.databind;
 
     opens belleisle.amiah.hardwarespecget to javafx.fxml;
     exports belleisle.amiah.hardwarespecget;
